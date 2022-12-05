4d93b1516f22caa99386fa3a524060d2a8177439
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-12-05 11:27:06 +0530

    Routing demo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// ----------------------
+
+// Route::get('/demo', function () {
+//     echo "hello world";
+// });
+
+Route::post('/test', function () {
+    echo "Testing the route";
 });
+
+// Route::get('/demo', function () {
+//     return view('demo');
+// });
+
+Route::get('/demo/{name}/{id?}', function ($name, $id = null) {
+    $data = compact('name','id');
+    return view('demo') -> with($data);
+});
+//http://127.0.0.1:8000/demo/Admin/011
