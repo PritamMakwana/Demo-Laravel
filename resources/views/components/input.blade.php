c6b3af397ed20d5ee8195e0ea2cc38a4dd9dbe0a
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-12-07 14:38:57 +0530

    components

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="mb-3">   
+          <label for="" class="form-label">{{$label}}</label>
+          <input type="{{$type}}" name="{{$name}}" id="" class="form-control border border-primary" placeholder="" aria-describedby="helpId" >
+          <span class="text-danger">
+            {{$demo}}
+          </span>
+        </div>
+
+</div>
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,73 +1,46 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-        <h1 class="text-center">Registeration</h1> 
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <form action="{{url('/')}}/regi" method="post">
-          @csrf
-          <!-- <pre>
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="text-center">Registeration</h1>
+
+    <form action="{{url('/')}}/regi" method="post">
+      @csrf
+      <!-- <pre>
           @php 
           print_r($errors->all());
           @endphp
           </pre> -->
 
-        <div class="form-group">
-        <div class="mb-3">   
-          <label for="" class="form-label">Name</label>
-          <input type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{old('name')}}">
-          <span class="text-danger">
-            @error('name')
-            {{$message}}
-            @enderror
-          </span>
-        </div>
+      @php
+      $demo = 1;
+      @endphp
 
-        <div class="mb-3">   
-          <label for="" class="form-label">Email</label>
-          <input type="email" name="email" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{old('email')}}">
-          <span class="text-danger">            
-            @error('email')
-            {{$message}}
-            @enderror</span>
-        </div>
+      <div class="form-group">
+        <x-input type="text" name="name" label="Name" :demo="$demo" />
+        <x-input type="email" name="email" label="Email" />
+        <x-input type="password" name="password" label="Password" />
+        <x-input type="password" name="password_confir" label="Confrim Password" />
 
-        <div class="mb-3">   
-          <label for="" class="form-label">Password</label>
-          <input type="password" name="password" id="" class="form-control" placeholder="" aria-describedby="helpId">
-          <span class="text-danger">
-          @error('password')
-            {{$message}}
-            @enderror
-          </span>
-        </div>
+        <button class="btn btn-primary"> Submit</button>
 
-        <div class="mb-3">   
-          <label for="" class="form-label">Confirm Password</label>
-          <input type="password" name="password_con" id="" class="form-control" placeholder="" aria-describedby="helpId">
-          <span class="text-danger">
-          @error('password_con')
-            {{$message}}
-            @enderror
-          </span>
-        </div>
+      </div>
+    </form>
+  </div>
 
-        <button class="btn btn-primary" > Submit</button>
 
-        </div>
-        </form>
-    </div>
+</body>
 
-    
-  </body>
 </html>
