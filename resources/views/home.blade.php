a85da14e0432116c44148d9bafeed45735bf8c03
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-12-15 16:58:04 +0530

    diff languages - Localization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
 @push('title') <title>Home </title> @endpush
 @section('main-section')
-<h1>Home</h1>
+<div class="d-flex flex-row">
+    <a href="{{url('/')}}" class="btn btn-primary m-1">English</a>
+    <a href="{{url('/hindi')}}" class="btn btn-danger m-1">Hindi</a>
+    <a href="{{url('/guj')}}" class="btn btn-warning m-1">Gujarati</a>
+</div>
+<h1>@lang('lang.welcome')</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\ProvisionServer;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,59 +20,59 @@ use Illuminate\Http\Request;
 
 // Insert
 //name router
-Route::get('/insert',[ProvisionServer::class,'index'])->name('insert');
-Route::post('/',[ProvisionServer::class,'store']);
+Route::get('/insert', [ProvisionServer::class, 'index'])->name('insert');
+Route::post('/', [ProvisionServer::class, 'store']);
 //select
-Route::get('/view',[ProvisionServer::class,'view'])->name('view');
+Route::get('/view', [ProvisionServer::class, 'view'])->name('view');
 
 //home page show
-Route::get('/',[RegistrationController::class,'index']);
+// Route::get('/',[RegistrationController::class,'index']);
+
+//language 
+Route::get('/{lang?}', function ($lang = null) {
+    App::setLocale($lang);
+    return view('home');
+});
 
 //delete
-Route::get('/delete/{id}',[ProvisionServer::class,'delete'])->name('delete');
+Route::get('/delete/{id}', [ProvisionServer::class, 'delete'])->name('delete');
 //update
-Route::get('/edit/{id}',[ProvisionServer::class,'edit'])->name('edit');
-Route::post('/update/{id}',[ProvisionServer::class,'update'])->name('update');
+Route::get('/edit/{id}', [ProvisionServer::class, 'edit'])->name('edit');
+Route::post('/update/{id}', [ProvisionServer::class, 'update'])->name('update');
 
 //session retrive
-Route::get('/get-all-session',function (){
+Route::get('/get-all-session', function () {
     $session = session()->all();
     p($session);
 });
 //session store
-Route::get('set-session',function (Request $req){
-    $req->session()->put('u_id','10');
-    $req->session()->put('u_name','user master');
-    $req->session()->flash('status','Success'); //tempary store
+Route::get('set-session', function (Request $req) {
+    $req->session()->put('u_id', '10');
+    $req->session()->put('u_name', 'user master');
+    $req->session()->flash('status', 'Success'); //tempary store
     return redirect('get-all-session');
 });
 //session destroy
-Route::get('destroy-session',function (){
+Route::get('destroy-session', function () {
     //1 
     // session()->forget('u_id');
     // session()->forget('u_name');
     //2
-    session()->forget(['u_id','u_name']);
+    session()->forget(['u_id', 'u_name']);
     return redirect('get-all-session');
 });
 
 //Trash
-Route::get('/trash',[ProvisionServer::class,'trash'])->name('trash');
+Route::get('/trash', [ProvisionServer::class, 'trash'])->name('trash');
 //restore
-Route::get('/restore/{id}',[ProvisionServer::class,'restore'])->name('restore');
+Route::get('/restore/{id}', [ProvisionServer::class, 'restore'])->name('restore');
 //parmanet delete
-Route::get('/force-delete/{id}',[ProvisionServer::class,'forceDelete'])->name('force-delete');
+Route::get('/force-delete/{id}', [ProvisionServer::class, 'forceDelete'])->name('force-delete');
 
 //laravel Form
-Route::get('/laravel-form',[ProvisionServer::class,'laravelForm']);
-Route::post('/laravel-form',[ProvisionServer::class,'formShow']);
+Route::get('/laravel-form', [ProvisionServer::class, 'laravelForm']);
+Route::post('/laravel-form', [ProvisionServer::class, 'formShow']);
 
 //gallery
-Route::get('/gallery',[ProvisionServer::class,'galleryShow']);
-Route::post('/gallery',[ProvisionServer::class,'galleryUplaod']);
-
-
-
-
-
-
+Route::get('/gallery', [ProvisionServer::class, 'galleryShow']);
+Route::post('/gallery', [ProvisionServer::class, 'galleryUplaod']);
